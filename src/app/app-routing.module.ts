99ef709b769353852350b97ce2fe18835b7068cb
jangjen98 <huonggiangtranthi1234@gmail.com>
2022-11-24 17:22:39 +0700

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+// ? Components
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RecipeBookComponent } from './pages/recipe-book/recipe-book.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'book',
+        pathMatch: 'full'
+      },
+      {
+        path: 'recipe',
+        component: RecipesComponent,
+      },
+      {
+        path: 'shop',
+        component: ShoppingListComponent,
+      },
+      {
+        path:'book',
+        component: RecipeBookComponent,
+      }
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// ? Modules
 import { AppRoutingModule } from './app-routing.module';
+
+// ? Components
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeListComponent } from './pages/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './pages/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './pages/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './pages/shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RecipeBookComponent } from './pages/recipe-book/recipe-book.component';
+
+const COMPONENTS = [
+  AppComponent,
+  HeaderComponent,
+  RecipesComponent,
+  RecipeListComponent,
+  RecipeDetailComponent,
+  RecipeItemComponent,
+  ShoppingListComponent,
+  ShoppingEditComponent,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [...COMPONENTS, NotFoundComponent, RecipeBookComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
